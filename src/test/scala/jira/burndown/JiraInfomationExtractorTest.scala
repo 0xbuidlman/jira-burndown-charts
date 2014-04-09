c0b0ec91d099a8a5c1c1cc7b2e85aa407668551c
Dan Brown <dbrown7@ebay.com>
2014-04-09 15:55:42 +0100

    Adding unit tests for moving averages

--- src/test/scala/jira/burndown/JiraInfomationExtractorTest.scala
+++ src/test/scala/jira/burndown/JiraInfomationExtractorTest.scala
@@ -1,6 +1,7 @@
 package jira.burndown
 
 import org.scalatest.{FlatSpec, FlatSpecLike, ShouldMatchers, FunSuite}
+import jira.burndown.jsonwrappers._
 
 class JiraInfomationExtractorTest extends FlatSpec
   with FlatSpecLike
@@ -10,6 +11,11 @@ class JiraInfomationExtractorTest extends FlatSpec
   val sprintFiveId: Int = 2837
   val projectId = 2676
   val projectName = "Large Merchant Integration"
+
+  val sprint1 = SprintBurndownSummary("Sprint 1", "", 2)
+  val sprint2 = SprintBurndownSummary("Sprint 2", "", 4)
+  val sprint3 = SprintBurndownSummary("Sprint 3", "", 6)
+  val sprint4 = SprintBurndownSummary("Sprint 4", "", 8)
 
   it should "give a project from the json" in {
     
@@ -41,5 +47,29 @@ class JiraInfomationExtractorTest extends FlatSpec
     //then
     sprintDetails.sprint.id should equal(sprintFiveId)
     sprintDetails.contents.completedIssuesEstimateSum.value should equal(18)
+  }
+  it should "calculate the average of 4 sprints with period 4" in {
+
+    //when
+    val averages = information.movingAverage(List(sprint1, sprint2, sprint3, sprint4), 4)
+
+    //then
+    val points = averages map { a => a.points }
+    points(0) should equal(2)
+    points(1) should equal(2.5)
+    points(2) should equal(3.5)
+    points(3) should equal(5)
+  }
+  it should "calculage averages of 4 sprints with a period of 2" in {
+
+    //when
+    val averages = information.movingAverage(List(sprint1, sprint2, sprint3, sprint4), 2)
+
+    //then
+    val points = averages map { a => a.points }
+    points(0) should equal(2)
+    points(1) should equal(3)
+    points(2) should equal(5)
+    points(3) should equal(7)
   }
 }
